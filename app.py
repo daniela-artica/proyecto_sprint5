71c9487af8ca2886215742185ea03d69220f7e08
Daniela A <stephap33@gmail.com>
2024-04-27 20:44:54 -0500

    cambio de path a ruta relativa

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import plotly_express as px
 import streamlit as st
-car_data = pd.read_csv(
-    "C:/Users/steph/Documents/python_projects/proyecto_sprint5/vehicles_us.csv")
+car_data = pd.read_csv("vehicles_us.csv")
 st.header("Datos de anuncios de coches")
 hist_button = st.button("Construir histograma")
 
 if hist_button:
-    st.write(
-        "Creación de un histograma para el conjunto de datos de anuncios de venta de coches")
+    st.write("Creación de un histograma para el conjunto de datos de anuncios de venta de coches")
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
 
